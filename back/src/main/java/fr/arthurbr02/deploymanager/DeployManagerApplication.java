c8bebd125969e710d00dd9659abbd4a557acc0ad
Arthur <arthurbratigny@gmail.com>
2026-04-26 13:56:02 +0200

    Correction serialization

--- back/src/main/java/fr/arthurbr02/deploymanager/DeployManagerApplication.java
+++ back/src/main/java/fr/arthurbr02/deploymanager/DeployManagerApplication.java
@@ -2,8 +2,10 @@ package fr.arthurbr02.deploymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class DeployManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeployManagerApplication.class, args);
